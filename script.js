aec0afcf560bc5c5153f658571536fab2d5254e6
Otieno Lazaro <otienolazaro111@gmail.com>
2025-10-05 07:32:31 +0300

    Fixing classlist issues

--- script.js
+++ script.js
@@ -35,7 +35,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const removeBtn = document.createElement('button');
         removeBtn.textContent = "Remove";
-        removeBtn.className = 'remove-btn';
+        removeBtn.classList.add('remove-btn'); 
+
 
         // Assign onclick event to remove task
         removeBtn.onclick = () => {
